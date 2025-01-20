272de8732888563bff9b66065b614bcf44547f0d
dawichcode <muvleltd@gmail.com>
2025-01-20 21:34:55 +0000

    added production confirguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.ts',
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'CrabJs',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  mode: 'production',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `/*!
+ * CrabJs v1.0.0
+ * (c) ${new Date().getFullYear()} Muvle Studio, inc
+ * Released under the MIT License.
+ * Muvle Nigeria Limited
+ * https://muvleltd.org
+ */`,
+      raw: true,
+    }),
+  ],
 }; 
